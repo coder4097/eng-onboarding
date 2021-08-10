02b703776436c44c14b8ca929220f1900fae4ed6
coder4097 <sourabh.patil@jalantechnologies.com>
2021-08-10 12:17:19 +0530

    fibonacci sequence | used prettier | changed title

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 // Fibonacci_sequesnce unto n terms
 
-const number = parseInt(prompt('Enter the number upto which you want fibonacci sequence: '));
-let firstNumber = 0, secondNumber = 1, nextNumber;
+const number = parseInt(
+  prompt(
+    "Enter the number (greater than zero) upto which you want fibonacci sequence: "
+  )
+);
+let firstNumber = 0,
+  secondNumber = 1,
+  nextNumber;
 
-console.log('Fibonacci Series:');
+console.log("Fibonacci Sequence:");
 
 for (let i = 1; i <= number; i++) {
-    console.log(firstNumber);
-    nextNumber = firstNumber + secondNumber;
-    firstNumber = secondNumber;
-    secondNumber = nextNumber;
+  console.log(firstNumber);
+  nextNumber = firstNumber + secondNumber;
+  firstNumber = secondNumber;
+  secondNumber = nextNumber;
 }
