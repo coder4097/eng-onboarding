f6486a213386dc108927ad9109676e082eaf905b
sourabh-JTC <88647737+coder4097@users.noreply.github.com>
2021-08-10 12:51:54 +0530

    finonacci-sequence upto 'n' terms (#1)
    * Update README.md
    
    * fibonacci sequence | used prettier | changed title
    
    * removed comments

---
+++ index.js
@@ -0,0 +1,17 @@
+const number = parseInt(
+  prompt(
+    "Enter the number (greater than zero) upto which you want fibonacci sequence: "
+  )
+);
+let firstNumber = 0,
+  secondNumber = 1,
+  nextNumber;
+
+console.log("Fibonacci Sequence:");
+
+for (let i = 1; i <= number; i++) {
+  console.log(firstNumber);
+  nextNumber = firstNumber + secondNumber;
+  firstNumber = secondNumber;
+  secondNumber = nextNumber;
+}
